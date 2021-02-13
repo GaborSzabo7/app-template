a71d632d4237c5bdbc5ed3c592d9f8ea338150a0
gaszabo <gaszabo7@gmail.com>
2021-02-13 13:28:04 +0100

    Add monitoring capabilities

--- src/main/java/hu/gaszabo/template/infrastructure/web/HttpMessageConverterConfiguration.java
+++ src/main/java/hu/gaszabo/template/infrastructure/web/HttpMessageConverterConfiguration.java
@@ -2,10 +2,12 @@ package hu.gaszabo.template.infrastructure.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -18,6 +20,11 @@ public class HttpMessageConverterConfiguration {
 	@Bean
 	public HttpMessageConverters customConverters() {
 		return new HttpMessageConverters((HttpMessageConverter<?>) new MappingJackson2HttpMessageConverter(objectMapper));
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
